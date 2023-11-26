30c8730950efa286cb569b6b6cc403703319e205
Alexey Korepanov <alexkorep@gmail.com>
2023-11-26 20:24:01 +0100

    Rename asteroid to meteoroid

--- src/scripts/Upgrade.gd
+++ src/scripts/Upgrade.gd
@@ -3,7 +3,7 @@ class_name Upgrade
 
 signal upgrade(body)
 
-export var title = "Title"
+export var title = "Title" setget set_title
 export var id = "ID"
 export var description = "Description"
 export var quantity = 0 setget set_quantity
@@ -21,14 +21,20 @@ func _ready():
 func get_recepie_text():
 	var text = ''
 	for item in recepie:
-		text += NumberFormatter.format_large_number(recepie[item]) + ' ' + item + '\n'
-	return text
+		if text != '':
+			text += ' + '
+		text += NumberFormatter.format_large_number(recepie[item]) + ' ' + item
+	return text + ' -> +1'
 
 func set_texture(value: Texture):
 	var TextureRect = $Panel/HBoxContainer/TextureRect
 	if value == null:
 		return
 	TextureRect.texture = value
+
+func set_title(value: String):
+	var TitleLabel = $Panel/HBoxContainer/Info/TitleLabel
+	TitleLabel.text = value
 
 func get_total_speed():
 	return speed*quantity
@@ -45,7 +51,7 @@ func set_speed(val: float):
 func set_quantity(val: int):
 	var QuantityLabel = $Panel/HBoxContainer/Info/QuantityLabel
 	quantity = val
-	QuantityLabel.text = NumberFormatter.format_large_number(quantity)
+	QuantityLabel.text = 'Qty: ' + NumberFormatter.format_large_number(quantity)
 
 func _on_Button_pressed():
 	# set_quantity(quantity + 1)
