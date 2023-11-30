aed6bb048515c63d730aec198ec5f4ea722348e3
Alexey Korepanov <alexkorep@gmail.com>
2023-11-30 20:29:13 +0100

    Updates

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,8 +3,8 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1701068733|42322848753";
-const CACHE_PREFIX = "Gravity's grasp-sw-cache-";
+const CACHE_VERSION = "1701372543|1474837591";
+const CACHE_PREFIX = "Gravity's Grasp-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
 // Files that will be cached on load.
