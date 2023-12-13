02bd3f7f508607c63cdf043272811e1d9e8a16c4
Alexey Korepanov <alexkorep@gmail.com>
2023-12-13 22:27:41 +0100

    Add a test ticker component

--- src/scripts/Upgrades.gd
+++ src/scripts/Upgrades.gd
@@ -6,7 +6,7 @@ signal on_first_upgrade(body)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	hide_upgrades_that_are_not_open_yet()
 
 func get_upgrades():
 	if not Upgrades:
@@ -19,7 +19,6 @@ func add_dust(amount):
 		if upgrade.body_type == upgrade.CelestialBody.DUST:
 			upgrade.quantity += amount
 
-
 func on_body_upgrade(body, qty):
 	print(body, qty)
 	var is_new_body_opened = false
@@ -31,5 +30,18 @@ func on_body_upgrade(body, qty):
 			is_new_body_opened = false
 	if is_new_body_opened:
 		emit_signal("on_first_upgrade", body)
+	hide_upgrades_that_are_not_open_yet()
 
-
+func hide_upgrades_that_are_not_open_yet():
+	var upgrades = get_upgrades()
+	var max_upgrade_with_quantity = 0
+	for i in range(upgrades.size()):
+		var upgrade = upgrades[i]
+		if upgrade.quantity > 0:
+			max_upgrade_with_quantity = i
+	for i in range(upgrades.size()):
+		var upgrade = upgrades[i]
+		if i > max_upgrade_with_quantity + 1:
+			upgrade.hide()
+		else:
+			upgrade.show()
