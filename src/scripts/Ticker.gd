eabe25667aaed38f22a2f356129d1b899b826bbe
Alexey Korepanov <alexkorep@gmail.com>
2023-12-13 23:05:40 +0100

    Implement smooth ticker scrolling

--- src/scripts/Ticker.gd
+++ src/scripts/Ticker.gd
@@ -1,33 +1,30 @@
 # Tickers the text, but goes character by character instead of smoothly, like the previous script.
-extends Label
+extends Control
 
-# All of the text to scroll
-export (String) var ticker_text = "Big news today! Godot is announced as best engine ever! "
-# Number of characters to display
-export (int) var num_chars_display = 20
+onready var ScrollContainer = $ScrollContainer
+onready var Label = $ScrollContainer/Label
+var scroll_pos = 0.0
 
-var current_char_pos = 0
-onready var text_length = ticker_text.length()
+export var scroll_step = 0.5
+export var text = 'text to scroll'
 
-# You could do this cleaner with a timer or something else, this is just quick and dirty
-var cumulative_delta = 0
+func _ready():
+	ScrollContainer = $ScrollContainer
+	ScrollContainer.get_h_scrollbar().rect_scale.x = 0
+	# Copy the text twice to make sure the beginning
+	# of the text is displayed after the ending of the text
+	Label.text = text + ' ' + text
+
 func _process(delta):
-	cumulative_delta += delta
-	if cumulative_delta > 0.1:
-		set_ticker_text()
-		cumulative_delta = 0
-	
-	
-func set_ticker_text():
-#	Set the text to the current position + some number of characters
-	text = ticker_text.substr(current_char_pos, num_chars_display)
-	
-#	If there is overflow (meaning that "end" of the string is at a higher index than the length of the ticker text)
-#	then loop back around and append the start of the text as needed
-	if current_char_pos + num_chars_display > ticker_text.length():
+	scroll_pos += scroll_step
+	ScrollContainer.scroll_horizontal = int(scroll_pos)
+	check_reset_scroll_pos()
 
-		text += ticker_text.substr(0, current_char_pos + num_chars_display - ticker_text.length())
-
-	current_char_pos += 1
-# 	Mod the char position so it goes back to zero
-	current_char_pos = current_char_pos % text_length
+func check_reset_scroll_pos():
+	var container_width = ScrollContainer.rect_size.x
+	var label_width = Label.rect_size.x
+	# Reset condition when the beginning of the second copy
+	# of the text is at the left edge of the control
+	if scroll_pos >= label_width/2:
+		scroll_pos = 0
+	
